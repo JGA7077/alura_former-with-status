fc45e6c857e9560dd509b042bb72315f24319aa2
João Araújo <joaogabriel-al@hotmail.com>
2022-04-15 17:49:39 -0300

    02 - 05.Tipografia e Switch

--- src/App.js
+++ src/App.js
@@ -1,14 +1,15 @@
 import { Component } from 'react';
 import FormularioCadastro from './components/FormularioCadastro/FormularioCadastro'
 import './App.css';
+import '@fontsource/roboto';
 
-import { Container } from '@mui/material';
+import { Container, Typography } from '@mui/material';
 
 class App extends Component {
   render() {
     return (
       <Container component="article" maxWidth="sm" >
-        <h1>Formulário de Cadastro</h1>
+        <Typography variant="h3" component="h3" align='center'>Formulário de Cadastro</Typography>
         <FormularioCadastro />
       </Container>
     );
--- src/components/FormularioCadastro/FormularioCadastro.jsx
+++ src/components/FormularioCadastro/FormularioCadastro.jsx
@@ -1,24 +1,25 @@
 import React from "react";
-import { Button, TextField } from "@mui/material";
+import { Button, TextField, Switch, FormControlLabel } from "@mui/material";
 
 function FormularioCadastro() {
   return (
     <form>
-      <TextField id="nome" label="Nome" variant="outlined" margin="normal" fullWidth/>
+      <TextField id="nome" label="Nome" variant="outlined" margin="normal" fullWidth />
 
-      <TextField id="sobrenome" label="Sobrenome" variant="outlined" margin="normal" fullWidth/>
+      <TextField id="sobrenome" label="Sobrenome" variant="outlined" margin="normal" fullWidth />
 
-      <TextField id="CPF" label="CPF" variant="outlined" margin="normal" fullWidth/>
+      <TextField id="CPF" label="CPF" variant="outlined" margin="normal" fullWidth />
 
-      <label>Promoções</label>
-      <input type="checkbox" />
+      <FormControlLabel label="Promoções" control={<Switch name="promocoes" defaultChecked color="primary" />} />
 
-      <label>Novidades</label>
-      <input type="checkbox" />
+      <FormControlLabel label="Novidades" control={<Switch name="promocoes" defaultChecked color="primary" />} />
+      
 
-      <Button variant="contained" color="primary" type="submit">Cadastrar</Button>
+      <Button variant="contained" color="primary" type="submit">
+        Cadastrar
+      </Button>
     </form>
-  )
+  );
 }
 
 export default FormularioCadastro;
