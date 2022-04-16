63e5d44271672cac0f8ffa09714724bb35befc1d
João Araújo <joaogabriel-al@hotmail.com>
2022-04-16 14:50:15 -0300

    03 - 02.Formulários não controlados

--- src/components/FormularioCadastro/FormularioCadastro.jsx
+++ src/components/FormularioCadastro/FormularioCadastro.jsx
@@ -2,17 +2,50 @@ import React from "react";
 import { Button, TextField, Switch, FormControlLabel } from "@mui/material";
 
 function FormularioCadastro() {
+  let nome = ''
   return (
-    <form>
-      <TextField id="nome" label="Nome" variant="outlined" margin="normal" fullWidth />
+    <form onSubmit={(event) => {
+      event.preventDefault()
+      console.log(nome)
+    }}>
+      <TextField 
+        
+        onChange={(event) => {
+          nome = event.target.value
+        }}
+        
+        id="nome" 
+        label="Nome" 
+        variant="outlined" 
+        margin="normal" 
+        fullWidth
+      />
 
-      <TextField id="sobrenome" label="Sobrenome" variant="outlined" margin="normal" fullWidth />
+      <TextField 
+        id="sobrenome" 
+        label="Sobrenome" 
+        variant="outlined" 
+        margin="normal" 
+        fullWidth 
+      />
 
-      <TextField id="CPF" label="CPF" variant="outlined" margin="normal" fullWidth />
+      <TextField 
+        id="CPF" 
+        label="CPF" 
+        variant="outlined" 
+        margin="normal" 
+        fullWidth 
+      />
 
-      <FormControlLabel label="Promoções" control={<Switch name="promocoes" defaultChecked color="primary" />} />
+      <FormControlLabel 
+        label="Promoções" 
+        control={<Switch name="promocoes" defaultChecked color="primary" />}
+      />
 
-      <FormControlLabel label="Novidades" control={<Switch name="promocoes" defaultChecked color="primary" />} />
+      <FormControlLabel 
+        label="Novidades" 
+        control={<Switch name="promocoes" defaultChecked color="primary" />} 
+      />
       
 
       <Button variant="contained" color="primary" type="submit">
