5b15a350afd98d8ef7f3e5d9d85eb0d29d0d9106
João Araújo <joaogabriel-al@hotmail.com>
2022-04-21 15:21:06 -0300

    02 - 05.Injeção de dependências

--- src/components/FormularioCadastro/DadosUsuario.jsx
+++ src/components/FormularioCadastro/DadosUsuario.jsx
@@ -1,11 +1,30 @@
 import React from "react";
 import { TextField, Button } from "@mui/material";
 
-function DadosUsuario(props) {
+function DadosUsuario({aoEnviar}) {
   return (
-    <form>
-      <TextField id="email" label="email" type="email" variant="outlined" margin="normal" fullWidth />
-      <TextField id="senha" label="senha" type="password" variant="outlined" margin="normal" fullWidth />
+    <form onSubmit={(event) => {
+      event.preventDefault()
+      aoEnviar()
+    }}>
+      <TextField 
+        id="email"
+        label="email"
+        type="email"
+        required
+        variant="outlined"
+        margin="normal" 
+        fullWidth 
+      />
+      <TextField 
+        id="senha" 
+        label="senha" 
+        required
+        type="password" 
+        variant="outlined" 
+        margin="normal" 
+        fullWidth 
+      />
       <Button variant="contained" color="primary" type="submit">
         Cadastrar
       </Button>
